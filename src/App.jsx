24c2b68a7d35dc8dc538f7444afc4fcfaa15fed0
kanish19 <kanishkani252@gmail.com>
2026-03-05 13:00:14 +0530

    RVTis here

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-purple-600">
+      <h1 className="text-5xl font-bold text-white drop-shadow-lg">
+        Hello World 🌟 
+        from  
+        kanish
+      </h1>
+    </div>
+  );
+}
